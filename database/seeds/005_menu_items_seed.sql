8b2832ab4e52dd1054df4ffc74c46f8c2a87cd45
JongUk <ringwp010@gmail.com>
2025-11-11 00:22:18 +0900

    feat: add example menu item seed data for Valentine Dinner

--- database/seeds/005_menu_items_seed.sql
+++ database/seeds/005_menu_items_seed.sql
@@ -1,29 +1,23 @@
 -- Seed data for menu_items table
--- This is a template - actual menu items should be added based on specific dinner requirements
--- Example structure for Valentine Dinner
+-- Example menu items for Valentine Dinner
 
--- Note: This seed file should be customized based on actual menu requirements
--- For now, we'll create a basic structure that can be expanded
-
--- Example: Valentine Dinner menu items
--- These would need to be customized based on actual menu requirements
--- INSERT INTO menu_items (dinner_id, name, default_quantity, unit, base_price, additional_price, is_required, can_remove, can_increase, can_decrease, max_quantity, min_quantity, ingredient_id, ingredient_quantity_per_unit)
--- SELECT 
---   d.id,
---   '스테이크',
---   1,
---   '개',
---   0,
---   50000,
---   true,
---   false,
---   true,
---   true,
---   3,
---   1,
---   (SELECT id FROM ingredients WHERE name = '고기'),
---   1
--- FROM dinners d WHERE d.name = 'Valentine Dinner';
-
--- This seed file is a placeholder and should be customized based on actual menu requirements
+-- Valentine Dinner: 스테이크 (필수 항목)
+INSERT INTO menu_items (dinner_id, name, default_quantity, unit, base_price, additional_price, is_required, can_remove, can_increase, can_decrease, max_quantity, min_quantity, ingredient_id, ingredient_quantity_per_unit)
+SELECT 
+  d.id,
+  '스테이크',
+  1,
+  '개',
+  0,
+  50000,
+  true,
+  false,
+  true,
+  true,
+  3,
+  1,
+  (SELECT id FROM ingredients WHERE name = '고기'),
+  1
+FROM dinners d WHERE d.name = 'Valentine Dinner'
+ON CONFLICT DO NOTHING;
 
